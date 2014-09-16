54e73a65e99fa80c0663b76b87e73ecdfd026590
Andrew Krioukov <krioukov@gmail.com>
2014-09-15 23:20:43 -0700

    Update comments

--- iOS/BLE Advertise/BLE Advertise/BluetoothBroadcast.m
+++ iOS/BLE Advertise/BLE Advertise/BluetoothBroadcast.m
@@ -38,7 +38,9 @@
     // Only two supported data types.
     // While app is in foreground, first 28 bytes of advertisingData go in initial advertisement.
     // If there is more data, the local name is put in a scan response.
-    // While app is in background, advertisingData is ignored. Apple-specific data is sent in the advertisement instead.
+    // While app is in background, advertisingData is ignored. Apple-specific data is sent in the advertisement instead:
+    // e.g. 14:FF:4C:00:01:00:00:80:00:00:00:00:00:00:00:00:00:00:00:00:00
+    
     NSDictionary *advertisingData = @{CBAdvertisementDataLocalNameKey:@"test",
                                       CBAdvertisementDataServiceUUIDsKey:@[self.uuid]};
     [self.peripheralManager startAdvertising:advertisingData];
