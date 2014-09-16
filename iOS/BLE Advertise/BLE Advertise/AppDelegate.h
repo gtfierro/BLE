2a939d8065adfd18dd57239191070296e3817d95
Andrew Krioukov <krioukov@gmail.com>
2014-09-15 21:00:53 -0700

    Initial Commit

---
+++ iOS/BLE Advertise/BLE Advertise/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BLE Advertise
+//
+//  Created by Andrew Krioukov on 9/15/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/BLE Advertise/BLE AdvertiseTests/BLE_AdvertiseTests.m
@@ -0,0 +1,34 @@
+//
+//  BLE_AdvertiseTests.m
+//  BLE AdvertiseTests
+//
+//  Created by Andrew Krioukov on 9/15/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BLE_AdvertiseTests : XCTestCase
+
+@end
+
+@implementation BLE_AdvertiseTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
